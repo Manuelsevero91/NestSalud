1c1f48ebc3f376a7d968f54e67ba22a07a1cc13d
Manuel <manusevero91@gmail.com>
2023-08-16 10:28:17 -0300

    Card nueva css

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -4,6 +4,7 @@ function Card ({ medico })  {
   
   return (
     <div className="card">
+      <h2>{medico.id}</h2>
       <h2>{medico.nombre}</h2>
       <h2>{medico.apellido}</h2>
       <p>Especialidad: {medico.especialidad}</p>
